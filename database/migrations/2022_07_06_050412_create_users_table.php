9e0a1e6035e08d5a828e5659ac5a8c441ecd0896
Camilo Hernandez <hrcamilo11@gmail.com>
2024-10-09 15:20:11 -0500

    Initial commit: Proyecto Restaurante

--- database/migrations/2022_07_06_050412_create_users_table.php
+++ database/migrations/2022_07_06_050412_create_users_table.php
@@ -13,16 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('Users', function (Blueprint $table) {
-            $table->id();
+        Schema::dropIfExists('users');
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('id_rol')
-                  ->onUpdate('cascade')
-                  ->onDelete('cascade')
-                  ->constrained('roles');
+            $table->bigInteger('id_rol')->unsigned();
             $table->rememberToken();
             $table->timestamps();
         });
